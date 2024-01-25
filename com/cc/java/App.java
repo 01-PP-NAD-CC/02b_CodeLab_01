162919acc1624f0ba638abd405c7fe68ddc3f6b6
nad1408 <155629463+nad1408@users.noreply.github.com>
2024-01-25 22:43:07 +0100

    Variante 1 und 2 ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,8 +7,28 @@ public class App {
         Konto Konto2 = new Konto();
         Konto Konto3 = new Konto();
 
-        System.out.println(Konto1.Kontostand);
-        System.out.println(Konto2.Kontostand);
-        System.out.println(Konto3.Kontostand);
+        //Varriante 1:
+
+
+        // System.out.println("Anfangs Kontostand: ");
+        // System.out.println("Konto1 Anfangs Kontostand: " + Konto1.Kontostand + "  | Aktueller Kontostand: " + (Konto1.Kontostand *= 2));
+        // System.out.println("Konto2 Anfangs Kontostand: " + Konto2.Kontostand + "  | Aktueller Kontostand: " + (Konto2.Kontostand *= 3));
+        // System.out.println("Konto3 Anfangs Kontostand: " + Konto3.Kontostand + "  | Aktueller Kontostand: " + (Konto3.Kontostand *= 10)); 
+
+
+        //Varriante 2:
+
+        
+        Konto1.Kontostand *= 2; // Verdoppelung
+        Konto2.Kontostand *= 3; // Verdreifachung
+        Konto3.Kontostand *= 10; // Verzehnfachung
+
+        System.out.println("Aktueller Kontostand:");
+        System.out.println("Konto1: " + Konto1.Kontostand);
+        System.out.println("Konto2: " + Konto2.Kontostand);
+        System.out.println("Konto3: " + Konto3.Kontostand);
+
+
+
     }
 }
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -7,4 +7,6 @@ public class Konto {
     public String Kontostand() {
         return this.Kontostand();
     }
+
+    
 }
